26b42a5a4f740909b8f4ce398eed9568e248a672
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-11-08 16:25:39 +0530

    Type Coertion and Truthy Falsy values

--- JS/Modules/script.js
+++ JS/Modules/script.js
@@ -27,3 +27,30 @@ f = "Hello"; // f is now a string
 console.log(typeof f); // "string"
 f = true; // f is now a boolean
 console.log(typeof f); // "boolean" 
+
+
+// type coercion
+let g = "5";
+let h = 10;
+let result = g + h;
+console.log(result); // "510" (string concatenation)
+result = h - g;
+console.log(result); // 5 (numeric subtraction)
+result = h * g;
+console.log(result); // 50 (numeric multiplication)
+result = h / g;
+console.log(result); // 2 (numeric division)
+result = h == g;
+console.log(result); // true (loose equality, type coercion occurs)
+result = h === g;
+console.log(result); // false (strict equality, no type coercion)
+console.log(2 * "nikhil"); // NaN (not a number) because "nikhil" cannot be coerced to a number
+// truthy and falsy values
+let values = [0, 1, "", "Hello", null, undefined, [], {}, NaN];
+values.forEach(value => {
+    if (value) {
+        console.log(`${value} is truthy`);
+    } else {
+        console.log(`${value} is falsy`);
+    }
+});
