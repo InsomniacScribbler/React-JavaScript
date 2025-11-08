adef5ee6874dddf222c69f9345819f0247d1f3c7
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-11-08 16:12:01 +0530

    Reference variables and primitive data types

--- JS/Modules/script.js
+++ JS/Modules/script.js
@@ -1,7 +1,19 @@
 // var, let, const -> Line by line comparision
 console.log(a); // undefined
 var a = 10;
-console.log(b); // ReferenceError
+//console.log(b); // ReferenceError
 let b = 20;
 const c = 30;
 console.log(a, b, c); 
+
+
+let d= {
+    name: "John",
+    
+}
+console.log(d.name); // "John"
+
+let e = d;
+e.name = "Doe";
+console.log(e.name); // "Doe"
+console.log(d.name); // "Doe"
