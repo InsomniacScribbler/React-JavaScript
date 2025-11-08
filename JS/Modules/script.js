04a7c84cdaeab0c5555238e65747261829adfcee
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-11-08 16:15:12 +0530

    Dynamic typing

--- JS/Modules/script.js
+++ JS/Modules/script.js
@@ -17,3 +17,13 @@ let e = d;
 e.name = "Doe";
 console.log(e.name); // "Doe"
 console.log(d.name); // "Doe"
+
+
+// Dynamic Typing : js allows to change 
+// //the type of a variable on the go not like other statically typed languages which means that once a variable is declared with a type, it can be changed in dynamic typing.
+let f = 100; // f is a number
+console.log(typeof f); // "number"
+f = "Hello"; // f is now a string
+console.log(typeof f); // "string"
+f = true; // f is now a boolean
+console.log(typeof f); // "boolean" 
